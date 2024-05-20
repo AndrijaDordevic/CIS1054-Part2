e936f5d97a458eb2346cef324d9892da9016e998
KarlCasssar <150530185+KarlCasssar@users.noreply.github.com>
2024-05-20 18:16:30 +0200

    Update index.php
    made the code dynamic

--- Code/index.php
+++ Code/index.php
@@ -1,4 +1,18 @@
 <?php
-    require_once __DIR__ . '/bootstrap.php';
+require_once __DIR__ . '/bootstrap.php';
+require_once __DIR__ . '/dbConnection.php';
 
-echo $twig-> render('index.html');
+$db = new dbConnection();
+
+//Get the menu items from the database
+$menuitems = $db->select('SELECT ItemID, Price, image, ItemName, ingredients, note FROM menuitems');
+
+//Check if there are any errors
+if ($menuitems === false) {
+    echo "Error fetching menu items: " . $db->error();
+    exit;
+}
+
+//Give the data to the html file
+echo $twig->render('index.html', ['menuitems' => $menuitems]);
+?>
