e921035fa3f0139812234f242ed0318144d99d51
Andrija <andrija.bbgdj@gmail.com>
2024-05-24 08:44:21 +0200

    Changed styling for contact and order

--- Code/submit.php
+++ Code/submit.php
@@ -14,13 +14,13 @@ function sanitize_input($input) {
 // Check if form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Sanitize input
-    $name = sanitize_input($_POST['name']);
-    $email = sanitize_input($_POST['email']);
-    $message = sanitize_input($_POST['message']);
+    $name = sanitize_input($_POST['Name']);
+    $email = sanitize_input($_POST['Email']);
+    $message = sanitize_input($_POST['Message']);
 
     $dsn = 'mysql:host=localhost;dbname=bakery';
     $username = 'Kim';
-    $password = '2po';
+    $password = '*81F9E2CEBE63285BC5BF55CC67DDF2C18373E851';
 
     try {
         $dbh = new PDO($dsn, $username, $password);
@@ -30,7 +30,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     // Prepare SQL statement with placeholders
-    $stmt = $dbh->prepare("INSERT INTO your_table_name (name, email, message) VALUES (:name, :email, :message)");
+    $stmt = $dbh->prepare("INSERT INTO contact (name, email, message) VALUES (:name, :email, :message)");
 
     // Bind parameters
     $stmt->bindParam(':name', $name);
