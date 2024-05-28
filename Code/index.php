82ccedcdaceff4198d4d0489e84a3a9d68999576
Andrija <andrija.bbgdj@gmail.com>
2024-05-28 17:20:21 +0200

    Removed dbconnection from index.php

--- Code/index.php
+++ Code/index.php
@@ -1,18 +1,7 @@
 <?php
 require_once __DIR__ . '/bootstrap.php';
-require_once __DIR__ . '/dbConnection.php';
 
-$db = new dbConnection();
-
-//Get the menu items from the database
-$menuitems = $db->select('SELECT ItemID, Price, image, ItemName, ingredients, note FROM menuitems');
-
-//Check if there are any errors
-if ($menuitems === false) {
-    echo "Error fetching menu items: " . $db->error();
-    exit;
-}
 
 //Give the data to the html file
-echo $twig->render('index.html', ['menuitems' => $menuitems]);
+echo $twig->render('index.html');
 ?>
