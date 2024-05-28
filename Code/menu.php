72599749650bc363206100508c123227e86b7c90
Gary Ken Micallef <gary.micallef.22@um.edu.mt>
2024-05-28 12:16:32 +0200

    Made changes to menu dropdown on navbar
    The menu dropdown now sends you to corresponding section on menu page, the dropdown items are now also dynamic. Also added a couple small changes to css

--- Code/menu.php
+++ Code/menu.php
@@ -4,15 +4,42 @@ require_once __DIR__ . '/dbConnection.php';
 
 $db = new dbConnection();
 
-//Get the menu items from the database
-$menuitems = $db->select('SELECT ItemID, Price, image, ItemName, ingredients, note,details FROM menuitems');
+// Get the menu items from the database
+$menuitems = $db->select('SELECT ItemID, CategoryID, Price, image, ItemName, ingredients, note, details FROM menuitems');
 
-//Check if there are any errors
-if ($menuitems === false) {
-    echo "Error fetching menu items: " . $db->error();
+// Get the categories from the database
+$categories = $db->select('SELECT CategoryID, CategoryName FROM menucategories');
+
+// Check if there are any errors
+if ($menuitems === false || $categories === false) {
+    echo "Error fetching data: " . $db->error();
     exit;
 }
 
-//Give the data to the html file
-echo $twig->render('menu.html', ['menuitems' => $menuitems]);
+$categoriesMap = [];
+foreach ($categories as $category) {
+    $categoriesMap[$category['CategoryID']] = $category['CategoryName'];
+}
+
+// Group menu items by category
+$menuItemsByCategory = [];
+foreach ($menuitems as $item) {
+    $categoryId = $item['CategoryID'];
+    if (!isset($menuItemsByCategory[$categoryId])) {
+        $menuItemsByCategory[$categoryId] = [];
+    }
+    $menuItemsByCategory[$categoryId][] = $item;
+}
+
+// Check if a section is specified in the URL
+$section = isset($_GET['section']) ? htmlspecialchars($_GET['section']) : '';
+
+if ($section) {
+    // Redirect to the section directly
+    header("Location: menu.php#$section");
+    exit();
+}
+
+// Render the menu page
+echo $twig->render('menu.html', ['menuItemsByCategory' => $menuItemsByCategory,'categoriesMap' => $categoriesMap]);
 ?>
